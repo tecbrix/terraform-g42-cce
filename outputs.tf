e391c3989d46443f92a48568208b528110c134d0
Naveed ur Rahman <naveed.rahman.ext@axa-gulf.com>
2022-03-10 23:13:36 +0400

    Added new outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "kube_config_raw" {
   value = g42cloud_cce_cluster.cluster.kube_config_raw
 }
+
+output "certificate_clusters" {
+  value = g42cloud_cce_cluster.cluster.certificate_clusters
+}
+
+output "certificate_users" {
+  value = g42cloud_cce_cluster.cluster.certificate_users
+}
